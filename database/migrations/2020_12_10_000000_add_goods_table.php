8a082a132d1307bbabb5a251aef432fc31548e92
lizhichao <lizhichao@qq.com>
2020-12-10 09:52:29 +0800

    商品数据库迁移，增加最低金额与销量

---
+++ database/migrations/2020_12_10_000000_add_goods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            $table->unsignedDecimal('min_price');
+            $table->unsignedInteger('sell_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('goods', function (Blueprint $table) {
+            //
+        });
+    }
+}
