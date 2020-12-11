5ac8115e1a9e6c5b34c0d44c7313ed04de165cd8
lizhichao <lizhichao@qq.com>
2020-12-11 10:35:58 +0800

    增加优惠券关联配置

--- src/Models/Traits/GoodsHasOther.php
+++ src/Models/Traits/GoodsHasOther.php
@@ -7,28 +7,10 @@ trait GoodsHasOther
 
     public function coupon()
     {
-        if (config('yzgoods.relationship.coupon')) {
-            switch (config('yzgoods.relationship.coupon.type')) {
-                case 'hasOne':
-                    return $this->hasOne(config('yzgoods.relationship.coupon.class'),
-                        config('yzgoods.relationship.coupon.foreignKey'));
-                    break;
-                case 'hasMany':
-                    return $this->hasMany(config('yzgoods.relationship.coupon.class'),
-                        config('yzgoods.relationship.coupon.foreignKey'));
-                    break;
-                case 'morphTo':
-                    return $this->morphTo(config('yzgoods.relationship.coupon.class'),
-                        config('yzgoods.relationship.coupon.foreignKey'));
-                    break;
-                case 'morphMany':
-                    return $this->morphMany(config('yzgoods.relationship.coupon.class'),
-                        config('yzgoods.relationship.coupon.foreignKey'));
-                    break;
-            }
-        } else {
-            return null;
-        }
+        $configs = config('yzgoods.relationship.coupon');
+        $type    = $configs['type'];
+
+        return $this->$type($configs['class'], $configs['foreignKey']);
     }
 
 }
